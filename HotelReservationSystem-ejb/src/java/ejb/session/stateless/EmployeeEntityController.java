532fa62281e4a1ed162150468dac79b2b84f8fa6
Lance Lan Meng <36537676+freeelancer@users.noreply.github.com>
2018-10-12 15:21:55 +0800

    added constructors for CustomerEntity

---
+++ HotelReservationSystem-ejb/src/java/ejb/session/stateless/EmployeeEntityController.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejb.session.stateless;
+
+import entity.EmployeeEntity;
+import javax.ejb.Local;
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import util.exception.EmployeeNotFoundException;
+import util.exception.InvalidLoginCredentialException;
+
+/**
+ *
+ * @author Lance
+ */
+@Stateless
+@Local(EmployeeEntityControllerLocal.class)
+@Remote(EmployeeEntityControllerRemote.class)
+public class EmployeeEntityController implements EmployeeEntityControllerRemote, EmployeeEntityControllerLocal 
+{
+    @PersistenceContext(unitName = "HotelReservationSystem-ejbPU")
+    private EntityManager em;
+
+    public EmployeeEntityController() {
+    }
+
+    @Override
+    public EmployeeEntity employeeLogin(String username, String password) throws InvalidLoginCredentialException
+    {
+        try
+        {
+            EmployeeEntity employeeEntity = retrieveEmployeeByUsername(username);
+            
+            if(employeeEntity.getPassword().equals(password))
+            {             
+                return employeeEntity;
+            }
+            else
+            {
+                throw new InvalidLoginCredentialException("Username does not exist or invalid password!");
+            }
+        }
+        catch(EmployeeNotFoundException ex)
+        {
+            throw new InvalidLoginCredentialException("Username does not exist or invalid password!");
+        }
+        
+    }
+    @Override
+    public EmployeeEntity retrieveEmployeeByUsername(String username) throws EmployeeNotFoundException
+    {
+        Query query = em.createQuery("SELECT s FROM EmployeeEntity s WHERE s.username = :inUsername");
+        query.setParameter("inUsername", username);
+        
+        try
+        {
+            return (EmployeeEntity)query.getSingleResult();
+        }
+        catch(NoResultException | NonUniqueResultException ex)
+        {
+            throw new EmployeeNotFoundException("Employee Username " + username + " does not exist!");
+        }
+    }
+    
+}
