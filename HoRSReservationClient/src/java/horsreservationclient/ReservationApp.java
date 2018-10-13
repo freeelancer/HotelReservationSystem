55aa880ffe69c721f0c50bcafa42f768b097c78e
thamwaikin <waikintham@hotmail.com>
2018-10-14 01:24:03 +0800

    update reservation app

--- HoRSReservationClient/src/java/horsreservationclient/ReservationApp.java
+++ HoRSReservationClient/src/java/horsreservationclient/ReservationApp.java
@@ -8,6 +8,8 @@ package horsreservationclient;
 import ejb.session.stateless.CustomerEntityControllerRemote;
 import entity.CustomerEntity;
 import java.util.Scanner;
+import util.exception.CustomerNotFoundException;
+import util.exception.UsernameExistException;
 import util.exception.InvalidLoginCredentialException;
 
 /**
@@ -35,10 +37,11 @@ public class ReservationApp {
         {
             System.out.println("*** Welcome to HoRS Reservation System ***\n");
             System.out.println("1: Login");
-            System.out.println("2: Exit\n");
+            System.out.println("2: Register");
+            System.out.println("3: Exit\n");
             response = 0;
             
-            while(response < 1 || response > 2)
+            while(response < 1 || response > 3)
             {
                 System.out.print("> ");
 
@@ -60,6 +63,17 @@ public class ReservationApp {
                 }
                 else if (response == 2)
                 {
+                    try
+                    {
+                        doRegistration();
+                    }
+                    catch(UsernameExistException ex)
+                    {
+                        System.out.println("Acount already exist: " + ex.getMessage() + "\n");
+                    }
+                }
+                else if (response == 3)
+                {
                     break;
                 }
                 else
@@ -67,13 +81,14 @@ public class ReservationApp {
                     System.out.println("Invalid option, please try again!\n");                
                 }
             }
-            
-            if(response == 2)
+  
+            if(response == 3)
             {
                 break;
             }
         }
     }
+     
     public void doLogin() throws InvalidLoginCredentialException
     {
         Scanner sc = new Scanner(System.in);
@@ -96,4 +111,60 @@ public class ReservationApp {
         }
     }
     
+    public void doRegistration() throws UsernameExistException
+    {
+        Scanner sc = new Scanner(System.in);
+        String username = "";
+        String password = "";
+        
+        System.out.println("*** Reservation System :: Registration ***\n");
+        
+        do 
+        {
+        System.out.print("Enter username> ");
+        username = sc.nextLine().trim();
+        } while(username.length() == 0);
+        
+        do
+        {
+        System.out.print("Enter password> ");
+        password = sc.nextLine().trim();
+        } while(password.length() == 0);
+        
+        try
+        {
+            currentCustomerEntity = customerEntityController.retrieveCustomerByUsername(username);
+            
+            throw new UsernameExistException("Customer username " + username + " does already exist!");
+        }
+        catch (CustomerNotFoundException ex)
+        {
+            System.out.println("*** Reservation System :: Registration Details ***\n");
+            
+            CustomerEntity newCustomerEntity = new CustomerEntity();
+            
+            newCustomerEntity.setUsername(username);
+            newCustomerEntity.setPassword(password);
+            
+            System.out.print("Enter first name> ");
+            newCustomerEntity.setFirstName(sc.nextLine().trim());
+            System.out.print("Enter last name> ");
+            newCustomerEntity.setLastName(sc.nextLine().trim());
+            System.out.print("Enter ID or Passport number> ");
+            newCustomerEntity.setIdNumber(sc.nextLine().trim());
+            System.out.print("Enter contact number> ");
+            newCustomerEntity.setContactNumber(sc.nextLine().trim());
+            System.out.print("Enter email> ");
+            newCustomerEntity.setEmail(sc.nextLine().trim());
+            System.out.print("Enter address line 1> ");
+            newCustomerEntity.setAddressLine1(sc.nextLine().trim());
+            System.out.print("Enter address line 2> ");
+            newCustomerEntity.setAddressLine2(sc.nextLine().trim());
+            System.out.print("Enter postal code> ");
+            newCustomerEntity.setPostalCode(sc.nextLine().trim());
+            
+            newCustomerEntity = customerEntityController.createNewCustomer(newCustomerEntity);
+            System.out.println("New account created successfully!: " + newCustomerEntity.getUsername() + "\n");
+        } 
+    }
 }
