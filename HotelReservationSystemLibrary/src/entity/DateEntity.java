8d809cd8ea4d9c632afa670861980f49c7eac980
thamwaikin <waikintham@hotmail.com>
2018-11-04 18:28:54 +0800

    added room exist exception

---
+++ HotelReservationSystemLibrary/src/entity/DateEntity.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entity;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Temporal;
+
+/**
+ *
+ * @author Wai Kin
+ */
+@Entity
+public class DateEntity implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long dateId;
+    @Temporal(javax.persistence.TemporalType.DATE)
+    private Date dateTime;
+    @ManyToOne
+    private RoomTypeEntity roomTypeEntity;
+
+    public DateEntity() {
+    }
+
+    public DateEntity(Date dateTime, RoomTypeEntity roomTypeEntity) {
+        this.dateTime = dateTime;
+        this.roomTypeEntity = roomTypeEntity;
+    }
+
+    public Long getDateId() {
+        return dateId;
+    }
+
+    public void setDateId(Long dateId) {
+        this.dateId = dateId;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public RoomTypeEntity getRoomTypeEntity() {
+        return roomTypeEntity;
+    }
+
+    public void setRoomTypeEntity(RoomTypeEntity roomTypeEntity) {
+        this.roomTypeEntity = roomTypeEntity;
+    }
+        
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (dateId != null ? dateId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the dateId fields are not set
+        if (!(object instanceof DateEntity)) {
+            return false;
+        }
+        DateEntity other = (DateEntity) object;
+        if ((this.dateId == null && other.dateId != null) || (this.dateId != null && !this.dateId.equals(other.dateId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "entity.DateEntity[ id=" + dateId + " ]";
+    }
+    
+}
